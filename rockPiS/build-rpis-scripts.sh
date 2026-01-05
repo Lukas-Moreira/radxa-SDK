169e7d49f16105831e5e002f01d58cb4e5c9b31e
sakanul <lukasmoreira@ufam.edu.br>
2026-01-05 12:04:54 -0400

    feat: Command to compile projects

---
+++ rockPiS/build-rpis-scripts.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# docker run --rm \
+#     -v "$(pwd)":/work \
+#     sdk-rpis-buster bash -lc '
+#         export SYSROOT=/work/sysroot
+#         aarch64-linux-gnu-gcc --sysroot="$SYSROOT" '$1' -o '$2'
+#     '
+
+set -e
+
+IMAGE="lukasmoreira/sdk-rpis-buster:v1.0"
+
+arg1="$1"
+arg2="$2"
+
+if [ -z "$arg1" ] || [ -z "$arg2" ]; then
+    echo "Usage: $0 <source_file.c> <output_binary>"
+    exit 1
+fi
+
+docker run --rm \
+    -v "$(pwd)":/work \
+    $IMAGE bash -lc "
+        export SYSROOT=/work/sysroot
+        aarch64-linux-gnu-gcc --sysroot=\"\$SYSROOT\" $arg1 -o $arg2
+    "
